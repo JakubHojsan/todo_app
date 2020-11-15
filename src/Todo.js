65b96a30d7a013b0a0758375dcfc1af504e49355
Jakub Hojsan <jakubhojsan@gmail.com>
2020-11-15 13:42:00 -0600

    Added a component

---
+++ src/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Todo() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Todo
+
+//React functional component w/ an export. 
+//This is the todo component
