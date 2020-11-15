384f2346fdfad3a3963394e1aba5214285b54c23
Jakub Hojsan <jakubhojsan@gmail.com>
2020-11-15 13:37:55 -0600

    Began Todo list project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+
+
 
 function App() {
+  const [todos, setTodos] = useState(['Let cat in!', 'Take out the garbage on Sunday!']);
+  const count = 0;
+  const [input, setInput] = useState('');
+  console.log('Input field:', input);
+
+  const addTodo = (event) => {
+    //onClick
+    event.preventDefault(); //Stops refresh to prevent short term memory allocation loss
+    console.log('Button Clicked');
+    setTodos([...todos, input]);
+    setInput(['']);
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Welcome to the <i>DO</i> list.</h1>
+
+      <form>
+        <FormControl>
+          <InputLabel>✔️ Write a Todo</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)}/>
+        </FormControl>
+
+        <Button disabled={!input}onClick={addTodo} variant="contained" color="primary">
+          Add Todo
+        </Button> 
+      </form> 
+
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
+
     </div>
   );
 }
