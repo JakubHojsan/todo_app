f3f25cdd57683311b9caf81d37735c5030759a0c
Jakub Hojsan <jakubhojsan@gmail.com>
2020-11-15 14:56:24 -0600

    Database functionality added using Firebase's Firestore (file ommited due to sensitive information)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Todo from './Todo';
 import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import db from './firebase';
+import firebase from 'firebase';
 
 
 
 function App() {
-  const [todos, setTodos] = useState(['Let cat in!', 'Take out the garbage on Sunday!']);
-  const count = 0;
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
-  console.log('Input field:', input);
+
+  //On load, listen to firebase DB and load new todos as they get created/deleted
+
+  useEffect(() => {
+    db.collection('todos').orderBy('timestamp', 'asc').onSnapshot(snapshot => {
+      console.log(snapshot.docs.map(doc => doc.data()));
+      setTodos(snapshot.docs.map(doc => doc.data().todo)) //snapshot map bubbles up as an arry, allowing 
+    }) 
+  }, [])
+
+  /*Fires on load ONLY (when database changes) 
+  Snapshot takes a "picture" on database change*/
+
 
   const addTodo = (event) => {
     //onClick
     event.preventDefault(); //Stops refresh to prevent short term memory allocation loss
-    console.log('Button Clicked');
-    setTodos([...todos, input]);
-    setInput(['']);
+
+    db.collection('todos').add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+
+    //setTodos([...todos, input]);
+    setInput('');
   } 
 
   return (
     <div className="App">
 
       <h1>Welcome to the <i>DO</i> list.</h1>
-
+      
       <form>
         <FormControl>
           <InputLabel>✔️ Write a Todo</InputLabel>
